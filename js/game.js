49ffcb0f98bab3f14b5f74a215789b67c0a055e4
Peter Kassenaar <info@kassenaar.com>
2015-04-18 22:08:20 +0200

    Basic Strategy, Doubling and Splitting implemented

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 /**
- * @author Scott Carroll (original: https://github.com/scottux/BlackJack)
  * @author Peter Kassenaar (fork: https://github.com/PeterKassenaar/blackjack-sim)
+ * @author Scott Carroll (original: https://github.com/scottux/BlackJack)
  */
 
 /** Play BLACKJACK! */
@@ -11,7 +11,11 @@ var BlackJack = (function ($) {
 	/* Set up our Game's Deck */
 	var deck;
 
-	/** Holds your Hand */
+	/** Holds all hands (including splitted hands) */
+	var hands = [];
+	var numSplitHands = 0;
+
+	/** Holds your current Hand */
 	var yourHand;
 
 	/** Holds dealer Hand */
@@ -31,19 +35,23 @@ var BlackJack = (function ($) {
 		$playerAmount = $('#playerAmount'),
 		$handsPlayed = $('#handsPlayed');
 
-	/* win/lose ratio */
+	/* win/lose ratio and other statistics */
 	var wins = 0;
 	var losses = 0;
 	var ties = 0;
 	var playerBlackjacks = 0;
 	var dealerBlackjacks = 0;
+	var winPercentage = 0;
+	var losePercentage = 0;
 
 	/* playerAmount & bets */
 	var playerAmount = 1000;
+	var minPlayerAmount = playerAmount;
+	var maxPlayerAmount = playerAmount;
 	var betSize = 50;
 	var units = playerAmount / betSize;
 	var handsPlayed = 0;
-	var isDoubled = false;
+	var isSplitted = false;
 
 	/********************/
 	/* Functions below here */
@@ -56,10 +64,15 @@ var BlackJack = (function ($) {
 		ties = 0;
 		playerBlackjacks = 0;
 		dealerBlackjacks = 0;
-		isDoubled = false;
+		isSplitted = false;
 		playerAmount = 1000; // TODO: read dynamically
+		minPlayerAmount = playerAmount;
+		maxPlayerAmount = playerAmount;
 		betSize = 50; // TODO: read dynamically
 		handsPlayed = 0;
+		numSplitHands = 0;
+		winPercentage = 0;
+		losePercentage = 0;
 		/* Make sure to shuffle at the start of the game. */
 		deck.shuffle();
 	};
@@ -70,6 +83,16 @@ var BlackJack = (function ($) {
 	 */
 	var checkBlackjack = function (hand) {
 		return ((hand.getHand().length === 2) && (hand.score() === 21));
+	};
+
+	/** Calculate percentage of value 1 from value 2 and return
+	 * percentage, rounded to 1 decimal.
+	 * @param val1, the smallest value
+	 * @param val2, the bigges value (from which the percentage is requested)
+	 * @returns {number} percentage, rounded to 1 decimal.
+	 */
+	var toPercentage = function (val1, val2) {
+		return Math.round((( val1 / val2) * 100) * 10) / 10;
 	};
 
 	/** Check if the hand has a pair (2,2, 3,3, 4,4 etc.).
@@ -89,10 +112,10 @@ var BlackJack = (function ($) {
 	};
 
 	/** Double down your bet */
-	var doubleDown = function () {
-		betSize *= 2;
-		isDoubled = true;
-	};
+	//var doubleDown = function () {
+	//	betSize *= 2;
+	//	isDoubled = true;
+	//};
 
 	/** Double down only allowed on first two cards
 	 * @returns {bool} true if doubling down is allowed
@@ -102,51 +125,87 @@ var BlackJack = (function ($) {
 		return hand.getHand().length === 2;
 	};
 
+	/** stand a hand. Called from AutoPlay
+	 *
+	 */
+	var stand = function () {
+		// Only play dealer hand if you're not busted
+		var dealerHasToPlay = false;
+		var playerHasBlackjack = false;
+		if (isSplitted) {
+			hands.shift(); // hand was splitted. Remove first hand (w/ the pair) from hands[]
+		}
+		for (var i = 0; i < hands.length; i++) {
+			yourHand = hands[i];
+			playerHasBlackjack = (hands.length === 1 && checkBlackjack(yourHand));
+			if (playerHasBlackjack || yourHand.score() <= 21) {
+				dealerHasToPlay = true;
+				break; // if dealer has to play, break no matter what.
+			}
+		}
+		if (dealerHasToPlay) {
+			dealDealerHand(playerHasBlackjack);
+		}
+		declareWinner(hands, dealerHand);// Compare hands and declare a winner per hand
+	};
+
 	/** Tally the score to determine the outcome. */
-	var declareWinner = function (userHand, dealerHand) {
-		var outcome = '',
-			dealerScore = dealerHand.score(),
-			userScore = userHand.score(),
-			userHasBlackjack = checkBlackjack(userHand),
-			dealerHasBlackjack = checkBlackjack(dealerHand);
+	var declareWinner = function (yourHands, dealerHand) {
+		for (var i = 0; i < yourHands.length; i++) {
+			var outcome = '',
+				result = '',
+				yourHand = yourHands[i],
+				dealerScore = dealerHand.score(),
+				userScore = yourHand.score(),
+				userHasBlackjack = checkBlackjack(yourHand),
+				dealerHasBlackjack = checkBlackjack(dealerHand);
 
-		/* I didn't make the rules, I just enforce them. */
-		if (userHasBlackjack && !dealerHasBlackjack) {
-			outcome = "You win: Blackjack!";
-			wins++;
-			playerBlackjacks++;
-			playerAmount += 1.5 * betSize;
-
-		} else if (dealerHasBlackjack && !userHasBlackjack) {
-			outcome = "You lose: dealer has blackjack!";
-			losses++;
-			dealerBlackjacks++;
-			playerAmount -= betSize;
-		} else if (dealerHasBlackjack && userHasBlackjack) {
-			outcome = "Tie: you both have blackjack!";
-			ties++;
-			dealerBlackjacks++;
-			playerBlackjacks++;
+			/* I didn't make the rules, I just enforce them. */
+			if (userHasBlackjack && !dealerHasBlackjack) {
+				outcome = "You win: Blackjack!";
+				wins++;
+				playerBlackjacks++;
+				playerAmount += 1.5 * yourHand.betSize();
+			} else if (dealerHasBlackjack && !userHasBlackjack) {
+				outcome = "You lose: dealer has blackjack!";
+				losses++;
+				dealerBlackjacks++;
+				playerAmount -= yourHand.betSize();
+			} else if (dealerHasBlackjack && userHasBlackjack) {
+				outcome = "Tie: you both have blackjack!";
+				ties++;
+				dealerBlackjacks++;
+				playerBlackjacks++;
+			}
+			else if (userScore > 21 || dealerScore === 21) {
+				outcome = "You lose!";
+				losses++;
+				playerAmount -= yourHand.betSize();
+			} else if (dealerScore > 21 || userScore === 21 || userScore > dealerScore) {
+				outcome = "You win!";
+				wins++;
+				playerAmount += yourHand.betSize();
+			} else if (dealerScore > userScore) {
+				outcome = "You lose!";
+				losses++;
+				playerAmount -= yourHand.betSize();
+			} else if (dealerScore === userScore) {
+				outcome = "You tied!";
+				ties++;
+			}
+			result += outcome + "<br />Dealer: " + dealerHand.score() + "<br />You: " + userScore;
+			/* Output the result of the round. */
 		}
-		else if (userScore > 21 || dealerScore === 21) {
-			outcome = "You lose!";
-			losses++;
-			playerAmount -= betSize;
-		} else if (dealerScore > 21 || userScore === 21 || userScore > dealerScore) {
-			outcome = "You win!";
-			wins++;
-			playerAmount += betSize;
-		} else if (dealerScore > userScore) {
-			outcome = "You lose!";
-			losses++;
-			playerAmount -= betSize;
-		} else if (dealerScore === userScore) {
-			outcome = "You tied!";
-			ties++;
+		// record stats and return resul
+		hands = []; // reset hands.
+		if (playerAmount > maxPlayerAmount) {
+			maxPlayerAmount = playerAmount;
 		}
-		/* Output the result of the round. */
+		if (playerAmount < minPlayerAmount) {
+			minPlayerAmount = playerAmount;
+		}
 		handsPlayed++;
-		return outcome + "<br />Dealer: " + dealerHand.score() + "<br />You: " + userScore;
+		return result;
 	};
 
 	/** Deal cards for the dealer. No strategy, Dealer must stand on 17 */
@@ -165,6 +224,15 @@ var BlackJack = (function ($) {
 				console.log('dealers score: ', dealerHand.score());
 			}
 		}
+	};
+
+	/** Split current hand into two seperate hands. It contains a pair.
+	 * @param yourHand
+	 */
+	var splitHand = function (yourHand) {
+		var currentHand = yourHand.getHand(); // currentHand now holds array w/ the pair.
+		hands.push(new Hand(deck, null, betSize, currentHand[0]));
+		hands.push(new Hand(deck, null, betSize, currentHand[1]));
 	};
 
 	/** autoPlay without any strategy.
@@ -196,7 +264,7 @@ var BlackJack = (function ($) {
 	};
 
 	/** autoPlay with Basic strategy, without doubling or splitting
-	 *  - uses the chart in basicStrategy.js
+	 *  - uses the chart in basicStrategyChart.js
 	 */
 	var autoPlayBasicStrategy = function () {
 		var rowNumber = 0;	// Total of own card
@@ -204,12 +272,11 @@ var BlackJack = (function ($) {
 		var decision = '';
 		var yourScore;
 
-		colNumber = dealerHand.score() - 2;// mapping to chart in basicStrategy.js
+		colNumber = dealerHand.score() - 2;// mapping to chart in basicStrategyChart.js
 		while (true) {
 			//  TODO: Further implement basic strategy chart / decisions here.
 			// 1. check for 21 or higher
 			if (yourHand.score() >= 21) {
-				$standButton.trigger('click');
 				break;
 			}
 
@@ -277,23 +344,29 @@ var BlackJack = (function ($) {
 				yourHand.hitMe();
 			}
 			else if (decision === 'DH') {
-				 //Double if allowed (only on first two cards), otherwise Hit.
+				//Double if allowed (only on first two cards), otherwise Hit.
 				if (canDouble(yourHand)) {
-					doubleDown();
+					yourHand.double();
 					yourHand.hitMe();
-					$standButton.trigger('click');
 					break;
 				} else {
 					yourHand.hitMe();
 				}
 			}
 			else if (decision === 'S' || decision === 'DS') {
-				$standButton.trigger('click');
 				break;
 			}
 			else if (decision === 'P') {
-				// TODO: implent splitting. For now: Hit
-				yourHand.hitMe();
+				// If hand is not splitted, split it. Re-splitting not allowed (=not implemented yet).
+				if (!isSplitted) {
+					splitHand(yourHand);
+					numSplitHands++;
+					isSplitted = true;
+					break;
+				} else {
+					// otherwise just hit.
+					yourHand.hitMe();
+				}
 			}
 		}
 	};
@@ -340,13 +413,8 @@ var BlackJack = (function ($) {
 	/********************/
 	/* Deal Button */
 	$dealButton.on('click', function () {
-		// reset flag isDoubled prior to dealing if necessary
-		if (isDoubled) {
-			betSize /= 2;
-			isDoubled = false;
-		}
 		$doubleButton.show();
-		yourHand = new Hand(deck, 2); // PK: two cards for the player
+		yourHand = new Hand(deck, 2, betSize); // PK: two cards for the player
 		dealerHand = new Hand(deck, 1); //PK: one card for the dealer.
 		updateUI();
 		showControls();
@@ -362,17 +430,6 @@ var BlackJack = (function ($) {
 		} else {
 			updateUI();
 		}
-	});
-
-	/* Stand Button */
-	$standButton.on('click', function () {
-		// Only play dealer hand if you're not busted
-		var playerHasBlackjack = checkBlackjack(yourHand);
-		if (playerHasBlackjack || yourHand.score() <= 21) {
-			dealDealerHand(playerHasBlackjack);
-		}
-		$yourHand.html(declareWinner(yourHand, dealerHand));// PK: Compare hands
-		showDeal();
 	});
 
 	/* Reset Button */
@@ -391,34 +448,70 @@ var BlackJack = (function ($) {
 		$standButton.trigger('click');
 	});
 
+	/* Stand Button */
+	$standButton.on('click', function () {
+		// Only play dealer hand if you're not busted
+		var dealerHasToPlay = false;
+		var playerHasBlackjack = false;
+		if (isSplitted) {
+			hands.shift(); // hand was splitted. Remove first hand (w/ the pair) from hands[]
+		}
+		for (var i = 0; i < hands.length; i++) {
+			yourHand = hands[i];
+			playerHasBlackjack = (hands.length === 1 && checkBlackjack(yourHand));
+			if (playerHasBlackjack || yourHand.score() <= 21) {
+				dealerHasToPlay = true;
+				break; // if dealer has to play, break no matter what.
+			}
+		}
+		if (dealerHasToPlay) {
+			dealDealerHand(playerHasBlackjack);
+		}
+		$yourHand.html(declareWinner(hands, dealerHand));// Compare hands and declare a winner per hand
+		showDeal();
+	});
+
 	/* Auto Play button */
 	$autoPlayButton.on('click', function () {
 		console.clear();
+		console.time('session');
 		var numHands = $('#numAutoPlayHands').val();
 		$resetButton.trigger('click');
 		for (var i = 0; i < numHands; i++) {
 			// reset hand and trigger new deal
 			console.info('New hand: # ' + i);
-			// reset flag isDoubled prior to dealing if necessary
-			if (isDoubled) {
-				betSize /= 2;
-				isDoubled = false;
-			}
-			yourHand = new Hand(deck, 2); // PK: two cards for the player
-			dealerHand = new Hand(deck, 1); //PK: one card for the dealer.
+			isSplitted = false;
+			hands.push(new Hand(deck, 2, betSize)); // New hand for player, two cards.
+			dealerHand = new Hand(deck, 1); // One card for dealer.
+			//***************************************
 			// check if user must stand or hit. Pick a strategy by (un)commenting.
-			 //autoPlayWithoutStrategy();
-			autoPlayBasicStrategy(); // HIER VERDER!
+			for (var j = 0; j < hands.length; j++) {
+				//autoPlayWithoutStrategy();
+				yourHand = hands[j];
+				autoPlayBasicStrategy();
+			}
+			//$standButton.trigger('click');
+			stand();
 		}
-		console.group('stats');
+		// calculate and log some derived stats
+		winPercentage = toPercentage(wins, numHands); // round to 1 decimal
+		losePercentage = toPercentage(losses, numHands);
+		console.group('Stats');
 		console.log('num Hands: ', numHands);
-		console.log('num wins: ', wins);
-		console.log('num losses: ', losses);
-		console.log('num ties: ', ties);
+		console.log('num wins: ', wins, '(', winPercentage, '%)');
+		console.log('num losses: ', losses, '(', losePercentage, '%)');
+		console.log('num ties: ', ties, '(', toPercentage(ties, numHands), '%)');
 		console.log('hands played: ', handsPlayed);
-		console.log('num player blackjacks: ', playerBlackjacks);
-		console.log('num dealer blackjacks: ', dealerBlackjacks);
-		console.groupEnd('stats');
+		console.log('num player blackjacks: ', playerBlackjacks, '(', toPercentage(playerBlackjacks, numHands), '%)');
+		console.log('num dealer blackjacks: ', dealerBlackjacks, '(', toPercentage(dealerBlackjacks, numHands), '%)');
+		console.log('num splitted hands: ', numSplitHands, '(', toPercentage(numSplitHands, numHands), '%)');
+		console.groupEnd('Stats');
+		// amount of money
+		console.group('Amount');
+		console.log('max player amount: ', maxPlayerAmount);
+		console.log('min player amount: ', minPlayerAmount);
+		console.log('current player amount: ', playerAmount);
+		console.groupEnd('Amount');
+		console.timeEnd('session');
 	});
-
 }(jQuery));
